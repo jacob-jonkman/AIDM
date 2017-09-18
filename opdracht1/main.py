edeeca0a54d387deccdf43bb1def21509c769ec9
Jacob <jacob.jonkman@gmail.com>
2017-09-18 15:19:44 +0200

    linear regression plus some minor fixes

--- opdracht1/main.py
+++ opdracht1/main.py
@@ -1,105 +1,89 @@
 import numpy as np
+from sklearn import linear_model
 
 maxRating = 5
 minRating = 1
 folds = 5
 
+# Compute the mean of all ratings in the data
 def naive_global(ratings):
 	return np.mean(ratings)
 
 
-def naive_item(data, num_movies):
+# Compute for each movie what the average rating of that movie is across all users
+def naive_item(data, num_movies, global_average_rating):
 	itemratings = np.zeros(num_movies)
 	itemcounts = np.zeros(num_movies)
 	
 	for i in np.arange(len(data)):
 		itemratings[data[i,1] - 1] += data[i,2]
 		itemcounts[data[i,1] - 1] += 1
-	
-	return np.round(itemratings/itemcounts, 1)
+		
+	return np.round([itemratings[j]/itemcounts[j] if itemcounts[j] > 0 else global_average_rating for j in range(num_movies)], 1)
 
 
-# Compute mean rating per user
-def naive_user(data, num_users):
+# Compute for each user what their average given rating is across all rated movies
+def naive_user(data, num_users, global_average_rating):
 	userratings = np.zeros(num_users)
 	usercounts = np.zeros(num_users)
 
-	for i in range(len(data)):
+	for i in np.arange(len(data)):
 		userratings[data[i,0]-1] += data[i,2]
 		usercounts[data[i,0]-1] += 1
-		
-	return np.round(userratings/usercounts, 1)
+	
+	return np.round([userratings[j]/usercounts[j] if usercounts[j] > 0 else global_average_rating for j in range(num_users)], 1)
 
 
-def naive_model(data, num_users, num_movies):#user, item):
-	def model(X, a, b, c):
-		x1, x2 = X
-		return a*x1 + b*x2 + c
-		
-	#Can be used to make a fit to a model
-	from scipy.optimize import curve_fit
-	#https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.curve_fit.html
-	#returns the best values for the parameters of the model in the array popt
-	#the array pcov contains the estimated covariance of popt
-	#p0 are the values for the variables it will start to look 
-	
-	x1 = naive_user(train_set, num_users)
-	x2 = naive_item(train_set, num_movies)
-	
-	print("hier!!!!!")
-	print(x1, x2)
-	
-	popt, pcov = curve_fit(model, (x1, x2), ydata, p0 = (1000., 1.))
-	#Then the standard deviation is given by:
-	perr = np.sqrt(np.diag(pcov))
-	#get the residuals:
-	residuals = ydata- model(xdata, popt)
-	#to get R^2:
-	ss_res = np.sum(residuals**2)
-	ss_tot = np.sum((ydata-np.mean(ydata))**2)
-	r_squared = 1 - (ss_res / ss_tot)
-	
-	
-
+# Use linear regression
+def naive_model(lin_reg_data, ratings):
+	reg = linear_model.LinearRegression()
+	reg.fit(lin_reg_data, ratings)	
+	return reg.coef_
 
 ## Squeeze ratings into range 1 to 5
 def roundRatings(ratings):
 	return(np.array([max(min(x, maxRating), minRating) for x in ratings]))
 
 
-def count_distinct(lst):
-	return np.max(lst)
-
-
 def main():
-	data = np.genfromtxt("ml-1m/ratings.dat", usecols=(0, 1, 2), delimiter='::', dtype='int', max_rows=100000)
+	## data = [userID, movieID, rating] ##
+	data = np.genfromtxt("ml-1m/ratings.dat", usecols=(0, 1, 2), delimiter='::', dtype='int')
 	
-	data[:,2] = roundRatings(data[:,2])
+	# Compute the number of users and movies in the data. This assumes that every movieID 
+	# and every userID lower than the biggest one in the data occurs at least once.
+	num_users = np.max(data[:,0])
+	num_movies = np.max(data[:,1])
+	print("users: ", num_users, "items:", num_movies)
+		
+	global_average_ratings = np.zeros(folds)
+	naive_user_ratings = np.zeros(num_users)
+	naive_item_ratings = np.zeros(num_movies)
 	
+	# Apply 5-fold cross validation
+	for fold in np.arange(folds):
+		print("fold", fold, end="")
+		
+		train_set = np.array([data[x] for x in np.arange(len(data)) if (x%5) != fold])
+		test_set = np.array([data[x] for x in np.arange(len(data)) if (x%5) == fold])
+		
+		global_average_ratings[fold] = naive_global(train_set[:,2])
+		naive_user_ratings += naive_user(train_set, num_users, global_average_ratings[fold])
+		naive_item_ratings += naive_item(train_set, num_movies, global_average_ratings[fold])
+		
+		print(" done")
+		
+	naive_user_ratings /= folds
+	naive_item_ratings /= folds
 	
+	# Construct lin_reg_data = [avg for user, avg for movie]
+	lin_reg_data = np.zeros((len(data), 2))
+	for i in range(len(data)):
+		lin_reg_data[i,0] = naive_user_ratings[data[i,0]-1]
+		lin_reg_data[i,1] = naive_item_ratings[data[i,1]-1]
+	
+	regression_coeffs = naive_model(lin_reg_data, data[:,2])
+	print("Linear Regression done. Coefficients:", regression_coeffs)
 
-	sum_naive_global = 0
-	sum_naive_item = 0
-	sum_naive_user = 0
-	sum_naive_model = 0
-	
-	num_users = count_distinct(data[:,0])
-	num_movies = count_distinct(data[:,1])
-	
-	for fold in range(folds):
-		train_set = np.array([data[x] for x in range(len(data)) if (x%5) != fold])
-		test_set = np.array([data[x] for x in range(len(data)) if (x%5) == fold])
-		
-		sum_naive_global += naive_global(train_set[:,2])
-		sum_naive_user += naive_user(train_set, num_users)
-		sum_naive_item += naive_item(train_set, num_movies)
-		sum_naive_model += naive_model(train_set, num_users, num_movies)
-		
-	print("mean of naive global classifier: ", sum_naive_global/folds)
-	print("means of naive user classifier: ", sum_naive_user/folds)
-	print("means of naive item classifier: ", sum_naive_item/folds)
-		
-		
-	
+
 if __name__ == "__main__":
 	main()
