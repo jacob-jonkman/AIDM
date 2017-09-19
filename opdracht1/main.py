e73089de8abb9972fecbcd90b2866e30d3fdb288
Jacob <jacob.jonkman@gmail.com>
2017-09-19 15:26:23 +0200

    wanhopige poging tot factorizatie

--- opdracht1/main.py
+++ opdracht1/main.py
@@ -5,6 +5,11 @@ maxRating = 5
 minRating = 1
 folds = 5
 
+num_factors = 10
+num_iter = 75
+regularization = 0.05
+learn_rate = 0.005
+
 # Compute the mean of all ratings in the data
 def naive_global(ratings):
 	return np.mean(ratings)
@@ -61,9 +66,53 @@ def applyNaiveModels(data, user_ratings, item_ratings, avg_ratings_list, global_
 	
 	return errors, avg_ratings_list
 
+def Xmatrix(data, num_users, num_movies):
+  #Convert the data set to the IxJ matrix  
+  X = np.zeros((num_users, num_movies)) * np.nan
+  for i in np.arange(len(data)):
+    X[data[i,0]-1,data[i,1]-1] = data[i,2]
+    
+  return X
+
+def matrixFact(data, num_users, num_movies):
+  #Convert the data set to the IxJ matrix  
+  X_data = Xmatrix(data, num_users, num_movies)
+  
+  X_hat = np.zeros(num_users, num_movies) #The 'weights', aka the expected values of the ratings
+  E = np.zeros(num_users, num_movies) #The error values 
+  #The matrices used to determine the ratings. These are initialized with random values and then converged to optimum values using gradient descent.
+  U = np.random.rand(num_users, num_factors) 
+  M = np.random.rand(num_factors, num_movies)
+  U_prime = np.zeros((num_users, num_factors))
+  M_prime = np.zeros((num_factors, num_movies))
+  
+  for q in range(num_iter):
+    X_hat = np.dot(U,M)
+    
+    E = X_data - X_hat
+    """
+    for i in np.arange(num_users):
+      for j in np.arange(num_movies):
+        for k in np.arange()
+        U_prime = U + learn_rate * (2. * E * M - regularization * U)
+        M_prime = M + learn_rate * (2. * E * U - regulalrization * M)
+      
+    
+    U = U_prime
+    M = M_prime
+    """
+  X_hat = np.dot(U,M)
+  
+  X_hat_errcalc = X_hat
+  X_hat_errcalc[np.where(X_data == np.nan)] = np.nan
+  
+  E = X_data - X_hat_errcalc
+  return X_hat
+      
+
 def main():
 	## Read dataset into data with format [userID, movieID, rating]
-	data = np.genfromtxt("ml-1m/ratings.dat", usecols=(0, 1, 2), delimiter='::', dtype='int')
+	data = np.genfromtxt("ml-1m/ratings.dat", usecols=(0, 1, 2), delimiter='::', dtype='int', max_rows=10000)
 	
 	# Compute the number of users and movies in the data. This assumes that every movieID 
 	# and every userID lower than the biggest one in the data occurs at least once.
@@ -88,6 +137,7 @@ def main():
 		train_set = np.array([data[x] for x in np.arange(len(data)) if (x % folds) != fold])
 		test_set = np.array([data[x] for x in np.arange(len(data)) if (x % folds) == fold])
 		
+		"""
 		# Compute the naive classifiers on the train set
 		global_average_rating = naive_global(train_set[:,2])
 		user_ratings = naive_user(train_set, num_users, global_average_rating)
@@ -106,16 +156,29 @@ def main():
 		
 		# Apply Linear Regression
 		regr_coeffs, regr_intercept = linearRegression(avg_ratings_list_train, train_set[:,2])
+		regression_predictions_train = roundRatings(regr_coeffs[0]*avg_ratings_list_train[:,0] + regr_coeffs[1]*avg_ratings_list_train[:,1] + regr_intercept)
+		regression_predictions_test = roundRatings(regr_coeffs[0]*avg_ratings_list_test[:,0] + regr_coeffs[1]*avg_ratings_list_test[:,1] + regr_intercept)
 		
-		
-		regr_error_train = np.sqrt(np.mean((train_set[:,2] - (regr_coeffs[0]*avg_ratings_list_train[:,0] + regr_coeffs[1]*avg_ratings_list_train[:,1] + regr_intercept))**2))
-		regr_error_test = np.sqrt(np.mean((test_set[:,2] - (regr_coeffs[0]*avg_ratings_list_test[:,0] + regr_coeffs[1]*avg_ratings_list_test[:,1] + regr_intercept))**2))
+		regr_error_train = np.sqrt(np.mean((train_set[:,2] - regression_predictions_train)**2))
+		regr_error_test = np.sqrt(np.mean((test_set[:,2] - regression_predictions_test)**2))
 		
 		print("Linear Regression done. Coefficients:", regr_coeffs, regr_intercept)
 		print("Training error:", regr_error_train)
 		print("Test error:", regr_error_test)
+		"""
 		
-		print()
+		X_hat = matrixFact(train_set, num_users, num_movies)
+		X_train = Xmatrix(train_set, num_users, num_movies)
+		X_test = Xmatrix(test_set, num_users, num_movies)
+		
+		E_train = X_train - X_hat
+		E_test = X_test - X_hat
+		
+		MF_error_train = np.sqrt(np.mean(E_train[np.where(E_train != np.nan)]**2))
+		MF_error_test = np.sqrt(np.mean(E_test[np.where(E_test != np.nan)]**2))
+		
+		print('MF training set error:', MF_error_train)
+		print('MF test set error:', MF_error_test)
 
 if __name__ == "__main__":
 	main()
