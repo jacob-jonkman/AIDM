a48b7f5bd7bef6036a998bed95df932d691f773d
J.W.F. Mes <s1530194@leidenuniv.nl>
2017-09-26 15:22:17 +0200

    Second cointoss try

--- opdracht2/main.py
+++ opdracht2/main.py
@@ -2,18 +2,40 @@ import numpy as np
 import loglogcounting as llc
 import cointoss as ct
 
-numrows = 1000
+numrows = 10
 numbits = 20
+
+def counttrue(bitstrings):
+	"""
+	Count the true number of distinct elements
+	"""
+	#list of all unique elements
+	unique = [bitstrings[0], bitstrings[1]]
+	print(unique)
+	for word in bitstrings:
+		for un in unique:
+			if np.sum(unique == word) == 0:
+				unique = np.append(unique, word)
+	
+	print(unique)
+	
+	return len(unique)
+	
+	
+	return 0
 
 def main():
 	np.set_printoptions(threshold=np.nan)
 	bitarray = np.random.randint(2, size=(numrows, numbits))
 	bitstrings = np.array([str(e).replace(' ','').replace('[','').replace(']','') for e in bitarray])
 	
-	llcount = llc.loglogcount(bitstrings)
-	#cointoss(bitstrings)
+	truecount = counttrue(bitstrings)
+	print('The true amount of unique elements:', truecount)
 	
-	print(bitstrings)
+	#llcount = llc.loglogcount(bitstrings)
+	#ctcount = ct.cointoss(bitstrings)
+	
+	#print(bitstrings)
 
 if __name__ == "__main__":
 	main()
