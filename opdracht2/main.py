44fbb59c6fd602fc78648c5ea9260289df5b072c
Jacob <jacob.jonkman@gmail.com>
2017-09-26 14:54:43 +0200

    genereer random bitstrings

--- opdracht2/main.py
+++ opdracht2/main.py
@@ -2,8 +2,18 @@ import numpy as np
 import loglogcounting
 import cointoss
 
+numrows = 1000
+numbits = 20
+
 def main():
-	pass
+	np.set_printoptions(threshold=np.nan)
+	bitarray = np.random.randint(2, size=(numrows, numbits))
+	bitstrings = ''.join(str(e).replace(' ','') for e in bitarray)
+	
+	loglogcount(bitstrings)
+	cointoss(bitstrings)
+	
+	print(bitstrings)
 
 if __name__ == "__main__":
 	main()
