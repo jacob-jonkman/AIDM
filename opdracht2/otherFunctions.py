a0ae15bab0866496d2f8a93f7c40a4199695dfd1
Jelle Mes <mes@pczaal14.strw.leidenuniv.nl>
2017-10-02 13:07:59 +0200

    Added function for displaying progress

---
+++ opdracht2/otherFunctions.py
@@ -0,0 +1,21 @@
+import sys
+
+def progress(count, total, suffix=''):
+	"""
+	Prints a progress bar in the terminal, indicating the progress in percentage\n
+	count (int):
+		Progress to date (iterable of a for loop for example)\n
+	total (int):
+		Max length of the work to be done (number of loops on for loop for example)\n
+	suffix = '' (str):
+		Suffix printed behind the progress bar
+	"""
+	
+	bar_len = 60
+	filled_len = int(round(bar_len * count / float(total)))
+		
+	percents = round(100.0 * count / float(total), 1)
+	bar = '=' * filled_len + '-' * (bar_len - filled_len)
+		
+	sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', suffix))
+	sys.stdout.flush()
