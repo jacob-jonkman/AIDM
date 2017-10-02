4f0b7941d273d6035d958641b4af113e66afaf65
Jelle Mes <mes@pczaal17.strw.leidenuniv.nl>
2017-10-02 16:41:40 +0200

    Improved the logging style; added two options here

--- opdracht2/main.py
+++ opdracht2/main.py
@@ -3,9 +3,16 @@ import loglogcounting as llc
 import cointoss as ct
 from timeit import default_timer
 import otherFunctions as oF
+import sys #for writing print statements to a log file
 
-numrows = int(1e6)
-numbits = 20
+numrows = int(1e5)
+numbits = 15
+
+#put all print statements in the log file. Set to false if you want only an
+#incomplete log, but get output in the terminal
+completelog = True
+if completelog:
+	sys.stdout = open("Log.txt", "w")
 
 def bitstringsToInt(bitstrings):
 	"""
@@ -53,20 +60,40 @@ def main():
 		print("Creating new bitstrings and saving them to file...")
 		bitarray = np.random.randint(2, size=(numrows, numbits))
 		bitstrings = np.array([str(e).replace(' ','').replace('[','').replace(']','') for e in bitarray])
-		np.save("./bitstrings_rows={0}_bits={1}.npy".format(numrows, numbits), bitstrings)
+		
+		#only save the data if it is not too large
+		if numrows * numbits < 5e7:
+			np.save("./bitstrings_rows={0}_bits={1}.npy".format(numrows, numbits), bitstrings)
+	
+	#open the log
+	if completelog == False:
+		log = open("Log.txt", "w")
+		log.write("\n\n------- New log -------\n")
+		log.write("numrows = {0}, numbits = {1}\n".format(numrows, numbits))
 	
 		#normal counting
 	starttime = default_timer()
 	truecount = counttrue(bitstrings, printprogress = True)
-	print('The true amount of unique elements:', truecount)
+	print('The true amount of unique elements: {0}'.format(truecount))
 	print('Runtime: {0} seconds'.format(round(default_timer() - starttime, 3)))
+	if completelog == False:
+		log.write('The true amount of unique elements: {0}\n'.format(truecount))
+		log.write('Runtime: {0} seconds\n'.format(round(default_timer() - starttime, 3)))
 	
 	#llcount = llc.loglogcount(bitstrings)
+	
 		#cointoss
 	starttime = default_timer()
 	ctcount = ct.cointoss(bitstrings, printprogress = True)
 	print("Cointoss count: {0}, Error: {1}%".format(ctcount, round(np.abs(truecount - ctcount)/truecount * 100., 3)))
 	print('Runtime: {0} seconds'.format(round(default_timer() - starttime, 3)))
+	if completelog == False:
+		log.write("Cointoss count: {0}, Error: {1}%\n".format(ctcount, round(np.abs(truecount - ctcount)/truecount * 100., 3)))
+		log.write('Runtime: {0} seconds\n'.format(round(default_timer() - starttime, 3)))
+		
+	if completelog == False:
+		log.close()
+	
 
 if __name__ == "__main__":
 	main()
--- opdracht2/otherFunctions.py
+++ opdracht2/otherFunctions.py
@@ -1,6 +1,6 @@
 import sys
 
-def progress(count, total, suffix=''):
+def progress(count, total, suffix='', printamount = 100):
 	"""
 	Prints a progress bar in the terminal, indicating the progress in percentage\n
 	count (int):
@@ -8,14 +8,15 @@ def progress(count, total, suffix=''):
 	total (int):
 		Max length of the work to be done (number of loops on for loop for example)\n
 	suffix = '' (str):
-		Suffix printed behind the progress bar
+		Suffix printed behind the progress bar\n
+		printamoutn = 1000 (int): the amount of times the progress is printed
 	"""
-	
-	bar_len = 60
-	filled_len = int(round(bar_len * count / float(total)))
+	if (count % int(total / printamount)) == 0:
+		bar_len = 60
+		filled_len = int(round(bar_len * count / float(total)))
 		
-	percents = round(100.0 * count / float(total), 1)
-	bar = '=' * filled_len + '-' * (bar_len - filled_len)
+		percents = round(100.0 * count / float(total), 1)
+		bar = '=' * filled_len + '-' * (bar_len - filled_len)
 		
-	sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', suffix))
-	sys.stdout.flush()
+		sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', suffix))
+		sys.stdout.flush()
