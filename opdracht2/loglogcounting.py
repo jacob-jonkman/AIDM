518672c88972a88d2e440d53818753cf56a00ae5
Jelle Mes <mes@pczaal14.strw.leidenuniv.nl>
2017-10-04 12:10:11 +0200

    Added the log and added prints for the loglog counting

--- opdracht2/loglogcounting.py
+++ opdracht2/loglogcounting.py
@@ -1,5 +1,6 @@
 import hashlib
 import numpy as np
+import otherFunctions as oF
 
 def rho(bitstring):
 	counter = 1
@@ -10,15 +11,24 @@ def rho(bitstring):
 	return counter-1
 
 
-def loglogcount(bitstrings, k):
+def loglogcount(bitstrings, k, printprogress = True):
+
+	print('\n--------- LogLog Count ---------')
+
 	buckets = 2**k
 	M = np.zeros(buckets, dtype=int)
 	alpha = 0.79402 # maar dan iets anders
 	
-	for bitstring in bitstrings:
-		j = int(bitstring[:k], 2)
-		r = rho(bitstring[k:])
+	looplength = len(bitstrings)
+	for i in np.arange(looplength):
+		if printprogress:
+			oF.progress(i, looplength)
+		j = int(bitstrings[i][:k], 2)
+		r = rho(bitstrings[i][k:])
 		M[j] = max(M[j], r)
 	
 	exponent = 2**(sum(M) / buckets)
+	
+	print('\n')
+	
 	return int(alpha * buckets * exponent)
