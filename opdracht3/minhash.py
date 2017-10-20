f314f9d3398004a7217b4dc8d90ee373bcd32e76
Jacob <jacob.jonkman@gmail.com>
2017-10-20 14:17:41 +0200

    poging tot lsh

--- opdracht3/minhash.py
+++ opdracht3/minhash.py
@@ -1,12 +1,33 @@
 import numpy as np
 
-bands = 100
+bands = 10
+
+def jaccard(matrix, user1, user2):
+	union = len(np.union1d(matrix[user1], matrix[user2]))
+	intersect = len(np.intersect1d(matrix[user1], matrix[user2]))
+	return intersect/union
+
+def dohash(row, num_hashes, bandwidth = 4):
+	return(np.sum(np.array([row[i:i+bandwidth]])**2) for i in np.arange(0, num_hashes, bandwidth))
 
 def min_hash(matrix, num_movies, num_users, num_hashes):
 	A = np.array([np.random.randint(0, num_movies) for i in np.arange(num_hashes)])
 	B = np.array([np.random.randint(0, num_movies) for i in np.arange(num_hashes)])
 	c = 2999
 	
+	"""sigMatrix = np.zeros(0)
+	
+	for i in np.arange(num_hashes):
+		permutation = np.random.permutation(matrix)
+		signatures = np.array([(A[i]*row + B[i]) % c for row in matrix])
+		sigMatrix = np.concatenate((sigMatrix, signatures))
+	
+	sigMatrix = np.reshape(sigMatrix, (num_users, num_hashes))
+	print("na reshape", sigMatrix.shape)
+	
+	print(np.min(sigMatrix))
+	
+	"""
 	sigMatrix = []
 	for row in matrix[1:]:
 		signature = []
@@ -21,11 +42,30 @@ def min_hash(matrix, num_movies, num_users, num_hashes):
 	
 	# sigMatrix bevat nu voor alle users die we hebben bekeken de signature row. #
 	#print(sigMatrix)
-	#print(len(sigMatrix))
+	print(len(sigMatrix))
 	
-	###
-	# Permute M randomly by
-	# p= randperm
-	# M[p,:]
-	###
+	buckets = {key: [] for key in np.arange(2**20)}
 	
+	for row, i in zip(sigMatrix, np.arange(len(sigMatrix))):
+		hashed_row = dohash(row, num_hashes)
+		for band in hashed_row:
+			if(buckets[band]):
+				buckets[band].append(i)
+			else:
+				buckets[band] = [i]
+	true = 0
+	false = 0
+	for k in buckets.keys():
+		if len(buckets[k]) > 1:
+			for i in np.arange(len(buckets[k])):
+				for j in np.arange(i+1, len(buckets[k])):
+					user1 = buckets[k][i]
+					user2 = buckets[k][j]
+					jaccardval = jaccard(matrix, user1, user2)
+					if jaccardval > 0.5:
+						print(user1, user2)
+						true += 1
+					else:
+						false += 1
+	print(true, false)
+
