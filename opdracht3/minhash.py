ee6113dd455559e05a1f59fac53228afd66992da
Jacob <jacob.jonkman@gmail.com>
2017-10-22 13:25:54 +0200

    do not compute jaccard for impossible pairs

--- opdracht3/minhash.py
+++ opdracht3/minhash.py
@@ -1,45 +1,23 @@
 import numpy as np
+from time import time
 
 bands = 10
 
 def jaccard(row1, row2):
 	union = len(np.union1d(row1, row2))
 	intersect = len(np.intersect1d(row1, row2))
-	if (intersect/union)>0.5:
-		print("union:", np.union1d(row1, row2), "intersect", np.intersect1d(row1, row2))
 	if union > 0:
 		return intersect/union
 	else: 
 		return 0
-
-"""def dohash(row, num_hashes, bandwidth = 4):
-	hashlist = []
-	for i in np.arange(0, num_hashes, bandwidth):
-		string = row[i:i+bandwidth]
-		hashlist.append(int(''.join(str(e) for e in string)))
-	return hashlist
-"""
 
 def min_hash(matrix, num_movies, num_users, num_hashes):
 	A = np.array([np.random.randint(0, num_movies) for i in np.arange(num_hashes)])
 	B = np.array([np.random.randint(0, num_movies) for i in np.arange(num_hashes)])
 	c = 2999
 	
-	"""sigMatrix = np.zeros(0)
-	
-	for i in np.arange(num_hashes):
-		permutation = np.random.permutation(matrix)
-		signatures = np.array([(A[i]*row + B[i]) % c for row in matrix])
-		sigMatrix = np.concatenate((sigMatrix, signatures))
-	
-	sigMatrix = np.reshape(sigMatrix, (num_users, num_hashes))
-	print("na reshape", sigMatrix.shape)
-	
-	print(np.min(sigMatrix))
-	
-	"""
 	sigMatrix = []
-	for row in matrix[1:1000]:
+	for row in matrix[1:]:
 		signature = []
 		
 		# For each row, find the signatures for all the different hashes #
@@ -51,16 +29,17 @@ def min_hash(matrix, num_movies, num_users, num_hashes):
 		sigMatrix.append(signature)
 		# sigMatrix bevat nu voor alle users die we hebben bekeken de signature row. #	
 
+	print("begin loop")
+	start_time = time()
 	# Hash bands to buckets #
 	buckets = {}	
-	for row, i in zip(sigMatrix, np.arange(len(sigMatrix))):
-		bands = np.split(np.array(row), 5)
+	for sigrow, i in zip(sigMatrix, np.arange(len(sigMatrix))):
+		bands = np.split(np.array(sigrow), 10)
 		for band in bands:
 			buckets.setdefault(tuple(band),[]).append(i)
 	
-	for k in buckets.keys():
-		print(buckets[k])
-	
+	print("Loop took %s seconds to execute" % (time() - start_time))
+		
 	true = 0
 	false = 0
 	for k in buckets.keys():
@@ -69,12 +48,12 @@ def min_hash(matrix, num_movies, num_users, num_hashes):
 				for j in np.arange(i+1, len(buckets[k])):
 					user1 = buckets[k][i]
 					user2 = buckets[k][j]
-					
-					jaccardval = jaccard(matrix[user1], matrix[user2])
-					if jaccardval > 0.5 and user1 != user2:
-						print(user1, user2, len(buckets[k]), k, buckets[k])
-						true += 1
-					else:
-						false += 1
+					if user1 != user2 and len(matrix[user1]) < 2*len(matrix[user2]) and len(matrix[user1]) > len(matrix[user2])/2:
+						jaccardval = jaccard(matrix[user1], matrix[user2])
+						if jaccardval > 0.5:
+							print(user1, user2, len(buckets[k]), k, buckets[k])
+							true += 1
+						else:
+							false += 1
 	print(true, false)
 
